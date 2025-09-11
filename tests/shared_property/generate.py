557f165538de13895468a215d5241c156e6e7a3b
Steven Fackler <sfackler@gmail.com>
2025-09-10 21:10:35 -0400

    make the test larger

--- tests/shared_property/generate.py
+++ tests/shared_property/generate.py
@@ -8,4 +8,4 @@ def test(n, name):
 test([{"a": 0, "b": 1} for i in range(10)], "ab")
 test([{str(i): 0} for i in range(100)] * 2, "large")
 test([{"repeated": 0, str(i): 1} for i in range(1300)], "evict")
-test([{str(i): 0, str(i + 1): 1} for i in range(256)], "valid_back_refs")
+test([{str(i): 0, str(i + 1): 1} for i in range(1300)], "valid_back_refs")
