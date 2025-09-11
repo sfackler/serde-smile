160bc0eeb162abda0a557b411d2dec64fd3181f0
Steven Fackler <sfackler@gmail.com>
2025-09-10 20:55:59 -0400

    Handle previously implicit back reference validity

--- src/ser/string_cache.rs
+++ src/ser/string_cache.rs
@@ -1,29 +1,39 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 
-const LIMIT: usize = 1024;
+const LIMIT: u16 = 1024;
 
 pub struct StringCache {
     map: HashMap<Cow<'static, str>, u16>,
+    next_idx: u16,
 }
 
 impl StringCache {
     pub fn new() -> Self {
         StringCache {
             map: HashMap::new(),
+            next_idx: 0,
         }
     }
 
     pub fn intern(&mut self, s: Cow<'static, str>) {
-        if self.map.len() >= LIMIT {
+        if self.next_idx >= LIMIT {
             self.map.clear();
+            self.next_idx = 0;
         }
 
-        let id = self.map.len() as u16;
-        self.map.insert(s, id);
+        let id = self.next_idx;
+        if valid_back_ref(id) {
+            self.map.insert(s, id);
+        }
+        self.next_idx += 1;
     }
 
     pub fn get(&mut self, s: &str) -> Option<u16> {
         self.map.get(s).copied()
     }
+}
+
+fn valid_back_ref(id: u16) -> bool {
+    id & 0xff < 0xfe
 }
